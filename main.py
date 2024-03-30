c8029ee63523c0fc2886313b68b9edeb362fdf70
пк <maxmax.228007228@gmail.com>
2024-03-31 00:22:42 +0300

    Первый коммит

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import render_template, Flask
+
+app = Flask(__name__)
+
+
+@app.route('/<title>')
+@app.route('/index/<title>')
+def index(title):
+    return render_template('index.html', title=title)
+
+
+if __name__ == "__main__":
+    app.run(port=8200, host="127.0.0.1")
