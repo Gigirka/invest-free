8efb6cbc591894971c6744669e693efb910906e7
пк <maxmax.228007228@gmail.com>
2024-04-06 15:44:09 +0300

    добавил возможность добавить изображение проекта (пока без бд)

--- main.py
+++ main.py
@@ -117,11 +117,11 @@ def add_job():
         jobs = Jobs(job=add_form.job.data,
                     team_leader=add_form.team_leader.data,
                     work_size=add_form.work_size.data,
-                    collaborators=add_form.collaborators.data,
-                    is_finished=add_form.is_finished.data)
+                    collaborators=add_form.collaborators.data)
+                    # is_finished=add_form.is_finished.data)
         db_sess.add(jobs)
         db_sess.commit()
-        return redirect('/')
+        return redirect("/")
     return render_template('add_job.html', title='Добавление работы',
                            form=add_form)
 
